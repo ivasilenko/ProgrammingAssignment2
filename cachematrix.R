1d4a31673965d0395024ad2a09f9c3c27904d4f1
Igor Vasilenko <i.vasilenko@gmail.com>
2015-03-22 17:58:40 +0200

    Added two functions
    Test case
    m <- matrix(c(-1, -2, 1, 1), 2,2)
    x <- makeCacheMatrix(m)
    x$get()
    inv <- cacheSolve(x)
    inv
    inv <- cacheSolve(x)
    inv

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Functions sets/gets matrix and its inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  im <- NULL
+  ##sets new matrix
+  set <- function(y) {
+    x <<- y
+    im <<- NULL
+  }
+  
+  ##gets current matrix
+  get <- function() x
+  
+  ## sets inverse matrix
+  setinverse <- function(data) im <<- data
+  
+  ## gets iverse matrix
+  getinverse <- function() im
+  
+  ## returns list of methods
+  list(set=set,
+       get=get,  
+       setinverse = setinverse,
+       getinverse = getinverse
+       )
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ##get inverse matrix
+  m <- x$getinverse()
+  ## if data is cached
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }  
+  ## inverse matrix and save
+  m <- solve(x$get())  
+  x$setinverse(m)
+  m
 }
+
